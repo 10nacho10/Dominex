9cbf60748648a24f2030927d6558763c7b7c7537
10nacho10 <49913676+10nacho10@users.noreply.github.com>
2020-03-03 00:34:19 -0300

    C:\Users\ignac\Desktop

---
+++ Domino.py
@@ -0,0 +1,14 @@
+import pytest 
+
+#
+
+
+
+# Representaremos los valores de entrada del domino como tuplas; siendo:
+# (numero1, numero2), (numero3, numnero3).
+# domino: Tuple Tuple -> True
+# Toma dos tuplas y entrega True si encanjan, en caso contrario entrega
+# False.
+# Entrada: (3, 4), (4, 5); Salida: True.
+# Entrada: (6, 6), (5, 5); Salida: False.
+# Entrada: (1, 2), (2, 7); Salida: True.
