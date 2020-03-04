acc95b492bec4fd2e37da8838f13ba061e0b4c3b
10nacho10 <49913676+10nacho10@users.noreply.github.com>
2020-03-04 17:10:35 -0300

    Se realizaron los comentarios y los casos de prueba, en la funcion: encajan

--- Domino.py
+++ Domino.py
@@ -1,14 +1,15 @@
 import pytest 
 
-#
-
-
-
 # Representaremos los valores de entrada del domino como tuplas; siendo:
 # (numero1, numero2), (numero3, numnero3).
-# domino: Tuple Tuple -> True
+# encajan: Tuple Tuple -> True
 # Toma dos tuplas y entrega True si encanjan, en caso contrario entrega
 # False.
 # Entrada: (3, 4), (4, 5); Salida: True.
 # Entrada: (6, 6), (5, 5); Salida: False.
-# Entrada: (1, 2), (2, 7); Salida: True.
+# Entrada: (2, 1), (2, 7); Salida: True.
+
+def test_encajan():
+    assert encajan((3, 4), (4, 5)) == True
+    assert encajan((6, 6), (5, 5)) == False
+    assert encajan((2, 1), (2, 7)) == True
