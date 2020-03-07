1d8fd99b69b22278000e0f9a1a0c6df4fd6759f9
10nacho10 <49913676+10nacho10@users.noreply.github.com>
2020-03-07 17:06:49 -0300

    Se agrego la funcion repatidor con sus comentarios/PD:La misma funciona de manera erronea. ERROR: ValueError: empty range for randrange() (0,0, 0)

--- Domino.py
+++ Domino.py
@@ -1,4 +1,5 @@
-import pytest 
+import pytest
+from random import *
 
 # Representaremos los valores de entrada del domino como tuplas; siendo:
 # (numero1, numero2), (numero3, numnero3).
@@ -55,7 +56,7 @@ def test_generacion():
     fichasDom = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6)]
     assert generacion() == fichasDom
 
-# Repreentaremos cada ficha como una tupla y al conjunto de fichas como una lista de tuplas.
+# Representaremos cada ficha como una tupla y al conjunto de fichas como una lista de tuplas.
 # buscador: Tuple, List -> Bool
 # Toma una ficha y una lista con fichas, devuelve un True si la ficha se encuentra en esa lista
 # de fichas, en caso contrario devuelve un False.
@@ -73,3 +74,132 @@ def test_buscador():
     assert buscador((3, 6), [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (1, 0)]) == False
     assert buscador((1, 1), [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (1, 1), (6, 6)]) == True
     assert buscador((5, 5), [(2, 0), (3, 1), (3, 2), (3, 3)]) == False
+
+# Representaremos las ficha como tupla y al conjunto de fichas como una lista de tuplas. Maximo
+# de jugadores son 
+# repartidor: Int Int List -> List
+# Toma la cantidad de jugadores, la cantidad de fichas y una lista de tupla, devuelve una lista con fichas aleatorias.
+# Entrada: 2, 3, [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (1, 0), (1, 1), (1, 2), (5, 4), (5, 5), (5, 6), (6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6)]; Salida: [[(6, 1), (6, 6), (1, 1)], [(5, 5), (0, 4), (5, 6)]].
+def repartidor(player, fichMax, listaFichas):
+    """Entrega una lista con las fichas de cada jugador."""
+    jugadores = []
+    jugador1 = []
+    jugador2 = []
+    jugador3 = []
+    jugador4 = []
+    jugador5 = []
+    jugador6 = []
+    jugador7 = []
+    n = 0
+    if player == 2:
+        while n != fichMax:
+            n += 1
+            random1 = randint(0, len(listaFichas)-1)
+            jugador1 += listaFichas[random1],
+            listaFichas[listaFichas.index(listaFichas[random1]):listaFichas.index(listaFichas[random1])+1] = []
+            random2 = randint(0, len(listaFichas)-1)
+            jugador2 += listaFichas[random2],
+            listaFichas[listaFichas.index(listaFichas[random2]):listaFichas.index(listaFichas[random2])+1] = []
+        jugadores += jugador1, jugador2,
+        return jugadores
+    elif player == 3:
+        while n != fichMax:
+            n += 1
+            random1 = randint(0, len(listaFichas)-1)
+            random2 = randint(0, len(listaFichas)-1)
+            random3 = randint(0, len(listaFichas)-1)
+            jugador1 += listaFichas[random1],
+            listaFichas[listaFichas.index(listaFichas[random1]):listaFichas.index(listaFichas[random1])+1] = []
+            jugador2 += listaFichas[random2],
+            listaFichas[listaFichas.index(listaFichas[random2]):listaFichas.index(listaFichas[random2])+1] = []
+            jugador3 += listaFichas[random3],
+            listaFichas[listaFichas.index(listaFichas[random3]):listaFichas.index(listaFichas[random3])+1] = []
+        jugadores += jugador1, jugador2, jugador3,
+        return jugadores
+    elif player == 4:
+        while n != fichMax:
+            n += 1
+            random1 = randint(0, len(listaFichas)-1)
+            random2 = randint(0, len(listaFichas)-1)
+            random3 = randint(0, len(listaFichas)-1)
+            random4 = randint(0, len(listaFichas)-1)
+            jugador1 += listaFichas[random1],
+            listaFichas[listaFichas.index(listaFichas[random1]):listaFichas.index(listaFichas[random1])+1] = []
+            jugador2 += listaFichas[random2],
+            listaFichas[listaFichas.index(listaFichas[random2]):listaFichas.index(listaFichas[random2])+1] = []
+            jugador3 += listaFichas[random3],
+            listaFichas[listaFichas.index(listaFichas[random3]):listaFichas.index(listaFichas[random3])+1] = []
+            jugador4 += listaFichas[random4],
+            listaFichas[listaFichas.index(listaFichas[random4]):listaFichas.index(listaFichas[random4])+1] = []
+        jugadores += jugador1, jugador2, jugador3, jugador4,
+        return jugadores
+    elif player == 5:
+        while n != fichMax:
+            n += 1
+            random1 = randint(0, len(listaFichas)-1)
+            random2 = randint(0, len(listaFichas)-1)
+            random3 = randint(0, len(listaFichas)-1)
+            random4 = randint(0, len(listaFichas)-1)
+            random5 = randint(0, len(listaFichas)-1)
+            jugador1 += listaFichas[random1],
+            listaFichas[listaFichas.index(listaFichas[random1]):listaFichas.index(listaFichas[random1])+1] = []
+            jugador2 += listaFichas[random2],
+            listaFichas[listaFichas.index(listaFichas[random2]):listaFichas.index(listaFichas[random2])+1] = []
+            jugador3 += listaFichas[random3],
+            listaFichas[listaFichas.index(listaFichas[random3]):listaFichas.index(listaFichas[random3])+1] = []
+            jugador4 += listaFichas[random4],
+            listaFichas[listaFichas.index(listaFichas[random4]):listaFichas.index(listaFichas[random4])+1] = []
+            jugador5 += listaFichas[random5],
+            listaFichas[listaFichas.index(listaFichas[random5]):listaFichas.index(listaFichas[random5])+1] = []
+        jugadores += jugador1, jugador2, jugador3, jugador4, jugador5,
+        return jugadores
+    elif player == 6:
+        while n != fichMax:
+            n += 1
+            random1 = randint(0, len(listaFichas)-1)
+            random2 = randint(0, len(listaFichas)-1)
+            random3 = randint(0, len(listaFichas)-1)
+            random4 = randint(0, len(listaFichas)-1)
+            random5 = randint(0, len(listaFichas)-1)
+            random6 = randint(0, len(listaFichas)-1)
+            jugador1 += listaFichas[random1],
+            listaFichas[listaFichas.index(listaFichas[random1]):listaFichas.index(listaFichas[random1])+1] = []
+            jugador2 += listaFichas[random2],
+            listaFichas[listaFichas.index(listaFichas[random2]):listaFichas.index(listaFichas[random2])+1] = []
+            jugador3 += listaFichas[random3],
+            listaFichas[listaFichas.index(listaFichas[random3]):listaFichas.index(listaFichas[random3])+1] = []
+            jugador4 += listaFichas[random4],
+            listaFichas[listaFichas.index(listaFichas[random4]):listaFichas.index(listaFichas[random4])+1] = []
+            jugador5 += listaFichas[random5],
+            listaFichas[listaFichas.index(listaFichas[random5]):listaFichas.index(listaFichas[random5])+1] = []
+            jugador6 += listaFichas[random6],
+            listaFichas[listaFichas.index(listaFichas[random6]):listaFichas.index(listaFichas[random6])+1] = []
+        jugadores += jugador1, jugador2, jugador3, jugador4, jugador5, jugador6
+        return jugadores
+    elif player == 7:
+        while n != fichMax:
+            n += 1
+            random1 = randint(0, len(listaFichas)-1)
+            random2 = randint(0, len(listaFichas)-1)
+            random3 = randint(0, len(listaFichas)-1)
+            random4 = randint(0, len(listaFichas)-1)
+            random5 = randint(0, len(listaFichas)-1)
+            random6 = randint(0, len(listaFichas)-1)
+            random7 = randint(0, len(listaFichas)-1)
+            jugador1 += listaFichas[random1],
+            listaFichas[listaFichas.index(listaFichas[random1]):listaFichas.index(listaFichas[random1])+1] = []
+            jugador2 += listaFichas[random2],
+            listaFichas[listaFichas.index(listaFichas[random2]):listaFichas.index(listaFichas[random2])+1] = []
+            jugador3 += listaFichas[random3],
+            listaFichas[listaFichas.index(listaFichas[random3]):listaFichas.index(listaFichas[random3])+1] = []
+            jugador4 += listaFichas[random4],
+            listaFichas[listaFichas.index(listaFichas[random4]):listaFichas.index(listaFichas[random4])+1] = []
+            jugador5 += listaFichas[random5],
+            listaFichas[listaFichas.index(listaFichas[random5]):listaFichas.index(listaFichas[random5])+1] = []
+            jugador6 += listaFichas[random6],
+            listaFichas[listaFichas.index(listaFichas[random6]):listaFichas.index(listaFichas[random6])+1] = []
+            jugador7 += listaFichas[random7],
+            listaFichas[listaFichas.index(listaFichas[random7]):listaFichas.index(listaFichas[random7])+1] = []
+        jugadores += jugador1, jugador2, jugador3, jugador4, jugador5, jugador6, jugador7
+        return jugadores
+    
